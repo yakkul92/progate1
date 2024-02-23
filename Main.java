5f45b450806e374ace40f610c6c6b6087fd0e94b
yakkul92 <sasapanda0902@gmail.com>
2024-02-23 23:00:00 +0900

    [create]scanner

--- Main.java
+++ Main.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args){
+        // // 文字列出力
+        // System.out.println("名前はKate Jonesです");
+        
+        // 文字入力→出力
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("名前：");
+        String firstName = scanner.next();
+        
+        System.out.print("苗字：");
+        String lastName = scanner.next();
+
+        System.out.println("名前は" + firstName + " " + lastName + "です");
+
+    }
 }
