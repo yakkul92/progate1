34cdfd913dd2e24c2658bcfcb6ab876b8678cdc2
yakkul92 <sasapanda0902@gmail.com>
2024-02-24 23:00:00 +0900

    [create]scanner

--- Main.java
+++ Main.java
@@ -13,7 +13,17 @@ public class Main {
         System.out.print("苗字：");
         String lastName = scanner.next();
 
+        System.out.print("年齢：");
+        int age = scanner.nextInt();
+
         System.out.println("名前は" + firstName + " " + lastName + "です");
+        System.out.println("年齢は" + age + "歳です");
+
+        if (age >= 20) {
+            System.out.println("成年者です");
+        }else{
+            System.out.println("未成年者です");
+        }
 
     }
 }
