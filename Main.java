b0513861b8c4124332c58acceba007f59c47c86e
yakkul92 <sasapanda0902@gmail.com>
2024-02-25 23:00:00 +0900

    [create]method

--- Main.java
+++ Main.java
@@ -16,6 +16,12 @@ public class Main {
         System.out.print("年齢：");
         int age = scanner.nextInt();
 
+        System.out.print("身長(m)");
+        double height = scanner.nextDouble();
+
+        System.out.print("体重(kg)");
+        double weight = scanner.nextDouble();
+
         System.out.println("名前は" + firstName + " " + lastName + "です");
         System.out.println("年齢は" + age + "歳です");
 
@@ -25,5 +31,25 @@ public class Main {
             System.out.println("未成年者です");
         }
 
+        printData(firstName, lastName, age, height, weight);
+
     }
+
+    public static void printData(String firstName, String lastName, int age, double height, double weight){
+        System.out.println("名前は" + fullName(firstName, lastName) + "です");
+        System.out.println("年齢は" + age + "歳です");
+
+        if (age >= 20) {
+            System.out.println("成年者です");
+        }else{
+            System.out.println("未成年者です");
+        }
+        System.out.println("身長は" + height + "mです");
+        System.out.println("体重は" + weight + "kgです");
+    }
+
+    public static String fullName(String firstName, String lastName){
+        return firstName + " " + lastName;
+    }
+
 }
