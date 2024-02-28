82a65f569b175011a8aa760c68165178a3768aaa
yakkul92 <sasapanda0902@gmail.com>
2024-02-28 23:00:00 +0900

    [create]person class

--- Main.java
+++ Main.java
@@ -31,25 +31,10 @@ public class Main {
             System.out.println("未成年者です");
         }
 
-        printData(firstName, lastName, age, height, weight);
+        Person.printData(firstName, lastName, age, height, weight);
 
     }
 
-    public static void printData(String firstName, String lastName, int age, double height, double weight){
-        System.out.println("名前は" + fullName(firstName, lastName) + "です");
-        System.out.println("年齢は" + age + "歳です");
 
-        if (age >= 20) {
-            System.out.println("成年者です");
-        }else{
-            System.out.println("未成年者です");
-        }
-        System.out.println("身長は" + height + "mです");
-        System.out.println("体重は" + weight + "kgです");
-    }
-
-    public static String fullName(String firstName, String lastName){
-        return firstName + " " + lastName;
-    }
 
 }
